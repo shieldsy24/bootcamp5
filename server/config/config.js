1a1877ab0a2eedd583dfdc566b9d6067ddfac2f3
shieldsy24 <shieldsy24@gmail.com>
2017-09-19 22:11:20 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://shieldsy24:Tony2014@ds133084.mlab.com:33084/bootcamp4', //place the URI of your mongo database here.
+  },
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
